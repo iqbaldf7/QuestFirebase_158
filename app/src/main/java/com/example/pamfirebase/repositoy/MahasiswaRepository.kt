cde461bb6f4f469bb74cf35e27ba292f1f53f10e
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-12 06:50:56 +0700

    melakukan penyesuain code & fungsi update/delete

--- app/src/main/java/com/example/pamfirebase/repositoy/MahasiswaRepository.kt
+++ app/src/main/java/com/example/pamfirebase/repositoy/MahasiswaRepository.kt
@@ -1,17 +1,17 @@
 package com.example.pamfirebase.repositoy
 
 import com.example.pamfirebase.model.Mahasiswa
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaRepository {
-
     suspend fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 
     suspend fun getMahasiswaByID(nim: String): Flow<Mahasiswa>
 
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
-    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 
-    suspend fun deleteMahasiswa(nim: String)
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
 }
