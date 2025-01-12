92f25a170c4d48ff39c4207ef9a37dfdbfa1d1a8
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-12 07:27:33 +0700

    melakukan penyesuaian code dan menambah fungsi formMahasiswa

--- app/src/main/java/com/example/pamfirebase/ui/theme/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pamfirebase/ui/theme/viewmodel/PenyediaViewModel.kt
@@ -1,5 +1,4 @@
 package com.example.pamfirebase.ui.theme.viewmodel
-
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
@@ -8,12 +7,10 @@ import com.example.pamfirebase.MahasiswaApplications
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiKontak().container.mahasiswaRepository) }
+        initializer { HomeViewModel(AplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(AplikasiMahasiswa().container.mahasiswaRepository) }
     }
-
 }
 
-
-
-fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+fun CreationExtras.AplikasiMahasiswa(): MahasiswaApplications =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
