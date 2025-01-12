a1ee74ba260027923dbd5eed05641e5d0b305d66
iqbaldf7 <Iqbaldathonah10@gmail.com>
2025-01-12 17:06:20 +0700

    penyesuaian pengelolaHalaman di mainActivity

--- app/src/main/java/com/example/pamfirebase/MainActivity.kt
+++ app/src/main/java/com/example/pamfirebase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pamfirebase.ui.theme.PamfirebaseTheme
+import com.example.pamfirebase.ui.theme.navigasi.PengelolaHalaman
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PamfirebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
